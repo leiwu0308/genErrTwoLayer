9734e1f5794e790fd074450b500d4d3a417087d2
leiwu <leiwu.pku@gmail.com>
2018-07-22 23:44:48 +0800

    back up since it will power off after midnight

--- submit_mnist.sh
+++ submit_mnist.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=6
 #SBATCH -t 20:00:00
 #SBATCH --gres=gpu:1
-#SBATCH -a 0-6
+#SBATCH -a 6-8
 
 width=(50 100 500 1000 5000 10000 50000 100000 500000)
 nepochs=(200 200 200 200 200 200 200 200 200)
@@ -13,5 +13,5 @@ nepochs=(200 200 200 200 200 200 200 200 200)
 python mnist.py --nepochs ${nepochs[$SLURM_ARRAY_TASK_ID]}\
          --width ${width[$SLURM_ARRAY_TASK_ID]} \
             --lr 0.1 --nsample 100 \
-            --lmbd 1e-5 --batchsize 10 
+            --lmbd 5e-5 --batchsize 10 
 
