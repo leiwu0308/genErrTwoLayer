1c6c7a6e81d33a13f84bf353955e431bff869dc4
leiwu <leiwu.pku@gmail.com>
2018-08-13 09:59:03 +0800

    clean up

--- mnist_train.py
+++ mnist_train.py
@@ -2,7 +2,6 @@ import time
 import os
 import argparse
 import math
-from copy import deepcopy
 import pickle
 
 import numpy as np
@@ -11,14 +10,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 from torch.optim import lr_scheduler
-from torch.autograd import Variable
-
-from torch.utils.data import DataLoader
-import torchvision.datasets as dsets
-import torchvision.transforms as trans
 
 from models import TwoLayerNet
 from trainer import *
@@ -34,6 +26,7 @@ argparser.add_argument('--nsamples', type=int, default=60000)
 argparser.add_argument('--batch_size',type=int,default=100)
 argparser.add_argument('--gpuid',default='0')
 argparser.add_argument('--weight_decay',type=float,default=0)
+argparser.add_argument('--plot',action='store_true')
 args = argparser.parse_args()
 os.environ['CUDA_VISIBLE_DEVICES']=args.gpuid
 
@@ -47,7 +40,7 @@ train_dl, test_dl = load_mnist(batch_size = args.batch_size,
 
 net = TwoLayerNet(784,args.width,10,args.initialize_factor)
 net = net.cuda()
-criterion = nn.MSELoss()
+criterion = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(net.parameters(),
                             lr = args.lr,weight_decay=args.weight_decay)
                             #,momentum=0.9,nesterov=True)
@@ -63,7 +56,6 @@ lmbd = lmbd_base * args.lmbd
 records = []
 
 print('lambda=%.2e'%(lmbd_base*args.lmbd))
-
 print('===> Start training our network .....')
 for epoch in range(args.nepochs):
     since = time.time()
@@ -77,41 +69,44 @@ for epoch in range(args.nepochs):
 
     now = time.time()
     records.append((tr_loss,tr_acc,te_loss,te_acc,net.path_norm().item(),total_loss))
-    print('[%3d/%d, %.0f seconds]| total_loss=%.2e,  tr_err: %.1e, tr_acc: %.2f |\t te_err: %.1e, te_acc: %.2f, pnorm: %.1e'%(
+    print('[%3d/%d, %.0f seconds]| total_loss=%.2e,  tr_err: %.1e, tr_acc: %.2f |
+        \t te_err: %.1e, te_acc: %.2f, pnorm: %.1e'%(
                 epoch+1,args.nepochs,now-since,
                 total_loss,tr_loss,tr_acc,te_loss,te_acc,net.path_norm().item()))
 print('===> End of training the network -----')
 print('the error of objective function is %.2e'%(total_loss))
 
-save_model(net,
-        'checkpoints/mnist_width:%d_nsamples:%d_lmbd:%.2e_teacc:%.2f_tracc:%.2f_.pkl'%(
-            args.width,args.nsamples,args.lmbd * lmbd_base, te_acc,tr_acc))
+file_prefix = 'mnist_wdth%d_lmbd%.3f_wd%.2e_lr%.1e_init%.1f_bz%d_totalLoss%.2e'%(
+                args.width,args.lmbd,args.weight_decay,args.lr,args.initialize_factor,
+                args.batch_size,total_loss)
+    )
+res = {'model_state':net.state_dict(),'learning_process':records}
+with open('checkpoints/%s.pkl'%(file_prefix),'wb') as f:
+    pickle.dump(res,f)
 
-records = np.asarray(records)
-plt.figure(figsize=(12,4))
-plt.subplot(1,3,1)
-plt.semilogy(records[:,0],label='train error')
-plt.semilogy(records[:,2],label='test error')
-plt.legend()
 
-plt.subplot(1,3,2)
-plt.plot(records[:,1],label='train accuracy')
-plt.plot(records[:,3],label='test accuracy: %.2f'%(te_acc))
-plt.legend()
 
-plt.subplot(1,3,3)
-plt.semilogy(records[:,4],label='path norm')
-plt.ylim([1,2e6])
-plt.legend()
 
-file_prefix = 'mnist_width%d_lmbd%.3f_lr%.1e_ifactor%.1f_bz%d_wd%.2e_totalLoss%.2e'%(
-                args.width,args.lmbd,args.lr,args.initialize_factor,args.batch_size,
-                args.weight_decay, tr_loss +args.lmbd * lmbd_base * net.path_norm().item()
-            )
-plt.savefig(
-    'figures/%s_.png'%(file_prefix),bbox_inches='tight'
-)
+if args.plot:
+    records = np.asarray(records)
+    plt.figure(figsize=(12,4))
+    plt.subplot(1,3,1)
+    plt.semilogy(records[:,0],label='train error')
+    plt.semilogy(records[:,2],label='test error')
+    plt.legend()
 
-with open('data/%s.pkl'%(file_prefix),'wb') as f:
-    pickle.dump(records,f)
+    plt.subplot(1,3,2)
+    plt.plot(records[:,1],label='train accuracy')
+    plt.plot(records[:,3],label='test accuracy: %.2f'%(te_acc))
+    plt.legend()
+
+    plt.subplot(1,3,3)
+    plt.semilogy(records[:,4],label='path norm')
+    plt.ylim([1,2e6])
+    plt.legend()
+
+    plt.savefig(
+        'figures/%s_.png'%(file_prefix),bbox_inches='tight'
+    )
+
 
